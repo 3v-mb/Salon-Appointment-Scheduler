ed46a2bb4f27098d52fb7adf1538c23a758c6778
3v-mb <139736780+3v-mb@users.noreply.github.com>
2023-08-17 15:49:30 +0530

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -32,11 +32,11 @@ APPOINT_SERVICE()
       read CUSTOMER_NAME
       ADD_CUSTOMER=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
     fi
-    echo -e "\nWhat time would you like your $SERVICE_CHOSEN, $CUSTOMER_NAME?"
+    echo -e "\nWhat time would you like your$SERVICE_CHOSEN,$CUSTOMER_NAME?"
     read SERVICE_TIME
-    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE name = '$CUSTOMER_NAME'")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
     APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-    echo -e "\nI have put you down for a$SERVICE_CHOSEN at $SERVICE_TIME, $CUSTOMER_NAME"
+    echo -e "\nI have put you down for a$SERVICE_CHOSEN at $SERVICE_TIME, $CUSTOMER_NAME."
   fi
 }
 
